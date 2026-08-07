e9380836c0e1aeeea7d11c76be08ef9b9eb86b80
tradinganalyse999-hash <tradinganalyse999@gmail.com>
2026-08-07 21:22:06 +0600

    Refactor buttons.py to improve menu structure

--- buttons.py
+++ buttons.py
@@ -24,15 +24,19 @@ def shop_menu():
 def product_menu(category):
     markup = InlineKeyboardMarkup(row_width=1)
     products = {
-        "vpn": [("Nord VPN 1 Month", 25.00),("Hma Vpn 7 Days", 30.00),("Proton Vpn 14 Day", 50.00),("Nord VPN 1 Month", 25.00),("Surfshark 7 Days", 25.00),("Hotspot Shield 7 Days", 25.00),("
-Cyber Ghost Vpn 3 Day", 20.00),("Avast 7 Days", 25.00),("Vpn - Viper 3 Days", 25.00),("Ip Vanish 7 Days", 25.00),("Pia 7 Days", 25.00),("Pure 7 Days", 25.00),("Potato 7 Days", 25.00),("Sky 7 Days", 25.00),("Turbo 7 Days", 25.00)],
+        "vpn": [
+            ("Nord VPN 1 Month", 25.00), ("Hma Vpn 7 Days", 30.00), ("Proton Vpn 14 Day", 50.00),
+            ("Surfshark 7 Days", 25.00), ("Hotspot Shield 7 Days", 25.00), ("Cyber Ghost Vpn 3 Day", 20.00),
+            ("Avast 7 Days", 25.00), ("Vpn - Viper 3 Days", 25.00), ("Ip Vanish 7 Days", 25.00),
+            ("Pia 7 Days", 25.00), ("Pure 7 Days", 25.00), ("Potato 7 Days", 25.00), 
+            ("Sky 7 Days", 25.00), ("Turbo 7 Days", 25.00)
+        ],
         "proxy": [("Owl Proxy 200MB", 10.00)],
-        "gmail": [("Gmail ", 50.00)],
+        "gmail": [("Gmail", 50.00)],
         "outlook": [("Outlook.com", 0.80)],
         "hotmail": [("Hotmail.com", 0.80)]
     }
     for name, price in products.get(category, []):
-        # IMPORTANT: | diye alada korsi jate name e space/_ thakleo problem na hoy
         markup.add(InlineKeyboardButton(f"🛒 {name} - 💎 {price} BDT", callback_data=f"select_qty|{category}|{name}|{price}"))
     markup.add(InlineKeyboardButton("⬅️ Back", callback_data="shop"))
     return markup
