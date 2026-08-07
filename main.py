566888ad2dafb2c7b6109e2d664c74754489e2a7
tradinganalyse999-hash <tradinganalyse999@gmail.com>
2026-08-07 17:06:46 +0600

    Pass bot instance to register_handlers function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import bot
 from handler import register_handlers
 
-register_handlers()
+register_handlers(bot)
 
 print("✅ ProxyStore BOT Started")
 bot.infinity_polling(timeout=30, skip_pending=True)
