00ad64e269c870ca603bc63cfe64bfcd52316803
tradinganalyse999-hash <tradinganalyse999@gmail.com>
2026-08-07 16:34:43 +0600

    Refactor bot initialization and handler registration

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 from config import BOT_TOKEN
+from handler import register_handlers
 
 bot = telebot.TeleBot(BOT_TOKEN)
-    from handler import register_handlers
-    register_handlers()
+register_handlers(bot)
