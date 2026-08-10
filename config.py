741885c2dd10aa47b18634def129d0355cdf727a
tradinganalyse999-hash <tradinganalyse999@gmail.com>
2026-08-11 00:55:13 +0600

    Update FORCE_JOIN_CHANNEL and FORCE_JOIN_LINK values

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ ADMIN_ID = int(os.environ.get("ADMIN_ID", "0"))
 SUPPORT_USERNAME = os.environ.get("SUPPORT_USERNAME", "@YourSupport")
 BOT_NAME = os.environ.get("BOT_NAME", "ProxyStore AI")
 
-FORCE_JOIN_CHANNEL = os.environ.get("FORCE_JOIN_CHANNEL", "@YourChannelUsername")
-FORCE_JOIN_LINK = os.environ.get("FORCE_JOIN_LINK", "https://t.me/YourChannelUsername")
+FORCE_JOIN_CHANNEL = os.environ.get("FORCE_JOIN_CHANNEL", "@Proxystore999")
+FORCE_JOIN_LINK = os.environ.get("FORCE_JOIN_LINK", "https://t.me/Proxystore999")
 
-# FORCE_JOIN_CHANNEL = "@TomarChannel"
-# FORCE_JOIN_LINK = "https://t.me/TomarChannel"
+# FORCE_JOIN_CHANNEL = "@Proxystore999"
+# FORCE_JOIN_LINK = "https://t.me/Proxystore999"
 
 BKASH_NUMBER = "01603940061"
 NAGAD_NUMBER = "01603940061"
