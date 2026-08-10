32b837d8d1a969d903db247679c81004fb8c92d3
tradinganalyse999-hash <tradinganalyse999@gmail.com>
2026-08-11 00:51:36 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-ADMIN_ID = 123456789 # Tomar ID
-SUPPORT_USERNAME = "@YourSupport"
-BOT_NAME = "ProxyStore AI"
+import os
 
-FORCE_JOIN_CHANNEL = "@Proxystore999"
-FORCE_JOIN_LINK = "https://t.me/Proxystore999"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "0"))
+SUPPORT_USERNAME = os.environ.get("SUPPORT_USERNAME", "@YourSupport")
+BOT_NAME = os.environ.get("BOT_NAME", "ProxyStore AI")
 
-PRICE_200MB = 60
-PRICE_1GB = 150
-PRICE_10GB = 500
-MORELOGIN_PRICE = 120
+FORCE_JOIN_CHANNEL = os.environ.get("FORCE_JOIN_CHANNEL", "@YourChannelUsername")
+FORCE_JOIN_LINK = os.environ.get("FORCE_JOIN_LINK", "https://t.me/YourChannelUsername")
 
-BKASH_NUMBER = "01XXXXXXXXX"
-NAGAD_NUMBER = "01XXXXXXXXX"
-ROCKET_NUMBER = "01XXXXXXXXX"
-USDT_ADDRESS = "YOUR_USDT_TRC20"
+# FORCE_JOIN_CHANNEL = "@TomarChannel"
+# FORCE_JOIN_LINK = "https://t.me/TomarChannel"
+
+BKASH_NUMBER = "01603940061"
+NAGAD_NUMBER = "01603940061"
+ROCKET_NUMBER = "off"
+USDT_ADDRESS = "TGE8oPaj7cYP14xuoHTZT19KxwSf12FYoz"
